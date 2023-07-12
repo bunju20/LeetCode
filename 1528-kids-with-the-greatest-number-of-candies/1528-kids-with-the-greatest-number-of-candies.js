a4396404d69791f7146879bda32beb0fed410da2
bunju20 <85238126+bunju20@users.noreply.github.com>
2023-07-12 15:41:48 +0900

    Time: 82 ms (5.51%), Space: 42.1 MB (72.52%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+const kidsWithCandies = (candies, extraCandies) => {
+  const ret = [];
+  let max = 0;
+  for (const val of candies) {
+    val > max && (max = val);
+  }
+  for (let i = 0; i < candies.length; ++i) {
+    ret.push(candies[i] + extraCandies >= max);
+  }
+  return ret;
+};
