845e62fdf763f2e591d9465de8a71b99e3510e59
bunju20 <85238126+bunju20@users.noreply.github.com>
2023-08-19 21:39:33 +0900

    Time: 88 ms (69.07%), Space: 51.1 MB (37.94%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function(root, key) {
+    
+    function callDFS(node) {
+        if(!node) return null;
+        if(node.val === key) {
+            if(!node.left) return node.right;
+            if(!node.right) return node.left;
+            let curr = node.right;
+            while(curr.left) curr = curr.left;
+            curr.left = node.left;
+            return node.right;
+        }
+        if(key > node.val) node.right = callDFS(node.right);
+        else node.left = callDFS(node.left);
+        return node;
+    }
+    return callDFS(root)
+};
